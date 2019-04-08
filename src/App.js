a72d9b631d112d58ad5670fe8521fc2a1062c02e
Steve Albonico <steve@albonico.net>
2019-04-07 17:20:18 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Slide from './Slide'
+import Cards from './Cards'
+import Logostuff from './Logostuff'
+import Accordion from './Accordion'
 
 class App extends Component {
+constructor(props){
+  super(props);
+  this.slideElement = React.createRef();
+}
+
+handleClick = () => {
+  this.slideElement.current.openLeftPanel()
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="thingtest">
+
+        </div>
+
+        <Cards />
+          <Slide ref={this.slideElement}/>
+
+        <div className="space"></div>
+
+        <Logostuff />
+
+
+
       </div>
     );
   }
